1b8721a2aae15a49d44a4b3b6630087f219ced92
IkramMohammad85 <ikramsonu@gmail.com>
2020-03-17 19:55:05 +0530

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -139,3 +139,9 @@ function getCookie(t) {
       return ""
 }
 
+$("#my-calendar").zabuto_calendar({
+      nav_icon: {
+        prev: '<i class="icon-long-arrow-left"></i>',
+        next: '<i class="icon-long-arrow-right"></i>'
+      }
+    });
